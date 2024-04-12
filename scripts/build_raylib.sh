7db4c0001c1535c7b4fe013c1266b11074c5b495
Jeremy Grosser <jeremy@synack.me>
2024-04-12 06:44:57 -0700

    let gpr resolve linker switches

--- scripts/build_raylib.sh
+++ scripts/build_raylib.sh
@@ -4,11 +4,12 @@ SRC="${RAYLIB_ALIRE_PREFIX}/raylib"
 BUILDDIR="${RAYLIB_ALIRE_PREFIX}/build-raylib"
 LIB="${BUILDDIR}/raylib/libraylib.a"
 
+mkdir -p ${BUILDDIR} || true
+
 if [ ! -e ${LIB} ]; then
-    mkdir ${BUILDDIR} || true
     cd ${BUILDDIR}
     cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=no ${SRC}
     make
 fi
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BUILDDIR}/raylib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BUILDDIR}/raylib
